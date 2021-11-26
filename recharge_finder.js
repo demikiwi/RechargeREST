05203b03a342f713799cdde747f6c16ce0e8a256
constann <nicolas.constandi@amersports.com>
2021-11-26 15:35:09 +0100

    1535

--- recharge_finder.js
+++ recharge_finder.js
@@ -14,6 +14,7 @@ module.exports = {
 
         return childPython.stdout.on('data', (data) => {
             output += data.toString()
+            console.log(output)
             return output
         })
 
